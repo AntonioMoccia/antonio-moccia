a425d9f8002c04ae8688c6f5bfa561d7de347c68
moccia.ant@gmail.com <moccia.ant@gmail.com>
2023-12-11 13:01:29 +0100

    added other props

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -7,14 +7,13 @@ import NextImage from '../NextImage'
 function Badge({ knowladge }: { knowladge: { title: string, level: number, technologylogo: { url: string, alt: string, dimensions: { width: number, height: number } } } }) {
 
     return (
-        <div className=' min-w-full   justify-between items-center px-5 py-3  text-white  bg-[rgb(34,34,34)] flex rounded-xl h-26'>
-            <div className='flex w-[100px] h-full flex-row gap-2 items-center justify-center'>
-                <NextImage image={knowladge.technologylogo} className='w-14 h-auto'  />
-                {<h1 className=''>{knowladge.title}</h1>}
+        <div className=' min-w-full justify-between items-center px-5 py-3  text-white  bg-[rgb(34,34,34)] flex rounded-xl h-26'>
+            <div className='  flex w-auto h-full flex-row gap-4 items-center justify-center'>
+                <NextImage image={knowladge.technologylogo} className='w-14 h-auto rounded-md' />
+                {<h1 className=' font-semibold'>{knowladge.title}</h1>}
             </div>
             <div className='h-full w-auto'>
                 <CircleProgress percentage={knowladge.level} circleWidth={60} />
-                {/*<p>{knowladge.level} / 10</p> */}
             </div>
         </div>
     )
