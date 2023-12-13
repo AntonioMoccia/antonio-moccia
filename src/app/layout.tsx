135a65b95c29f422874ce01cefe029a67a5af89a
moccia.ant@gmail.com <moccia.ant@gmail.com>
2023-12-13 08:59:25 +0100

    added Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className=' bg-[rgb(22,21,19)]'>{children}</body>
+      <body className=' bg-[rgb(22,21,19)]'>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
